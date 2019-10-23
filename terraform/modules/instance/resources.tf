450cfc1ab12c221a023dcfb5f86eb8077f1feb74
bhanuraina <bhanuraina@gmail.com>
2019-10-23 20:23:29 +0200

    Update resources.tf

--- terraform/modules/instance/resources.tf
+++ terraform/modules/instance/resources.tf
@@ -4,23 +4,56 @@ provider "aws" {
   region     = "${var.region}"
 }
 
+resource "aws_security_group" "sg_22" {
+  name = "sg_22"
+  vpc_id = "${var.vpc_id}"
+
+  # SSH access from the VPC
+  ingress {
+      from_port   = 22
+      to_port     = 22
+      protocol    = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+}
+
+resource "aws_security_group" "sg_80" {
+  name = "sg_80"
+  vpc_id = "${var.vpc_id}"
+
+  ingress {
+      from_port   = 80
+      to_port     = 80
+      protocol    = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+}
 resource "aws_instance" "instance" {
+  count         = "${var.instance_count}"
   ami           = "${var.instance_ami}"
   instance_type = "${var.instance_type}"
+	tags = {
+    Name  = "WEBSERVER-${count.index + 1}"
+      }
+
   subnet_id = "${var.subnet_public_id}"
-  vpc_security_group_ids = ["${var.security_group_ids}"]
+  vpc_security_group_ids=["${aws_security_group.sg_22.id}"]
   key_name = "${var.key_pair_name}"
-
-  tags {
-		"Environment" = "${var.environment_tag}"
-	}
 }
 
-resource "aws_eip" "testInstanceEip" {
-  vpc       = true
-  instance  = "${aws_instance.instance.id}"
-
-  tags {
-    "Environment" = "${var.environment_tag}"
-  }
-}
